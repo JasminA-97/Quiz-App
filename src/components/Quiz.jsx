37c2e07456c48ce72701934fbc97f332d123867e
JasminA-97 <21mp1828@rit.ac.in>
2024-05-29 13:58:23 +0530

    Quiz App using ReactJS

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { data } from '../assets/data';
+import quizLogo from '../assets/quizLogo.png'
 
 const Quiz = () => {
   const [index, setIndex] = useState(0);
@@ -8,8 +9,17 @@ const Quiz = () => {
   const [score, setScore] = useState(0);
   const [result, setResult] = useState(false);
   const [congrats, setCongrats] = useState(false);
+  
 
   const optionRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+
+  useEffect(() => {
+    if (score === data.length) {
+      setCongrats(true);
+    } else {
+      setCongrats(false);
+    }
+  }, [score]);
 
   const checkAns = (ans) => {
     if (!lock) {
@@ -39,44 +49,44 @@ const Quiz = () => {
     }
   };
 
-  useEffect(() => {
-    if (score === data.length) {
-      setCongrats(true);
-    } else {
-      setCongrats(false);
-    }
-  }, [score]);
+  const handleReset = () =>{
+    window.location.reload();
+  }
 
   return (
     <>
       <div style={{ height: '100vh' }} className="container d-flex justify-content-center align-items-center flex-column">
-        <div className='border ps-5 pe-5 pt-2 pb-4 pt-4 bg-light w-50'>
-          <h1 className='text-center'>Quiz App</h1>
-          <hr className='pb-2' />
+        <div className='border ps-5 pe-5 pt-2 pb-4 pt-4 bg-light w-50 border rounded-4'>
+          <h1 className='text-center space-mono-bold-italic pt-3 ps-3 pe-3 pb-2  bg-light'><img height={'60px'} width={'60px'} src={quizLogo} alt="" />uiz App</h1>
+          <hr />
 
           {!result && (
             <>
-              <div>{index + 1}/ {data.length}</div>
-              <h5>{index + 1}. {question.question}</h5>
-              <div className='d-flex justify-content-center flex-column mt-4 pt-3'>
-                <h6 onClick={() => checkAns(1)} ref={optionRefs[0]} className={`border rounded-4 p-3`}>{question.option1}</h6>
-                <h6 onClick={() => checkAns(2)} ref={optionRefs[1]} className={`border rounded-4 p-3`}>{question.option2}</h6>
-                <h6 onClick={() => checkAns(3)} ref={optionRefs[2]} className={`border rounded-4 p-3`}>{question.option3}</h6>
-                <h6 onClick={() => checkAns(4)} ref={optionRefs[3]} className={`border rounded-4 p-3`}>{question.option4}</h6>
+              <h5 className='pt-3'>{index + 1}. {question.question}</h5>
+              <div style={{cursor:'pointer'}} className='d-flex justify-content-center flex-column mt-4 pt-3'>
+                <h6 onClick={() => checkAns(1)} ref={optionRefs[0]} className={`border rounded-4 p-3`}>A. {question.option1}</h6>
+                <h6 onClick={() => checkAns(2)} ref={optionRefs[1]} className={`border rounded-4 p-3`}>B. {question.option2}</h6>
+                <h6 onClick={() => checkAns(3)} ref={optionRefs[2]} className={`border rounded-4 p-3`}>C. {question.option3}</h6>
+                <h6 onClick={() => checkAns(4)} ref={optionRefs[3]} className={`border rounded-4 p-3`}>D. {question.option4}</h6>
               </div>
 
-              <div className='text-center mt-3'><button onClick={handleNext} className="btn btn-primary">Next</button></div>
+              <div  className='text-center mt-3'><button style={{cursor:'default'}} onClick={handleNext} className="btn btn-primary ps-4 pe-4"><i className="fa-solid fa-forward"></i></button></div>
+              <div style={{fontSize:'14px'}} className='text-center mt-4'>{index + 1} of {data.length} questions</div>
             </>
           )}
 
           {result &&
             <div className='text-center'>
-              <p>Quiz completed.</p>
+              <p className=' fs-5 pt-3'>Quiz completed!!!</p>
               <p>Total Number of questions : {data.length}</p>
-              <p>Your Score : {score}/{data.length}</p>
+              <p className='fs-4 fw-bolder text-success'>Your Score : {score}/{data.length}</p>
+
               {congrats &&
-                <div className="text-success fs-3 fw-bolder">Congratulations!</div>
+                <div style={{fontSize:'60px'}} className="text-warning p-4 fw-bolder satisfy-regular">Congratulations!</div>
               }
+
+<div  className='text-center mt-4'><button style={{cursor:'default'}} onClick={handleReset} className="btn btn-primary  ps-4 pe-4"><i className="fa-solid fa-rotate-right "></i></button></div>
+
             </div>
           }
         </div>
